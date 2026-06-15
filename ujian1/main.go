0602430e727ef79d1d9b6bde0a16893905d3fc23
rucci <d.rucci.2001@gmail.com>
2026-06-15 16:36:33 +0700

    ujian1

---
+++ ujian1/main.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+day5-abstract-struktur-data-dinamis.pdf
+Sesi 6: Soal Ujian Akhir Modul (Studi Kasus HRIS)
+Studi Kasus: Sistem Manajemen Kepegawaian & Payroll Terintegrasi
+*/
+
+func main() {
+
+}
