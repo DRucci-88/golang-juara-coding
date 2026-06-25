5618c02545766fa686ae0fddaaa7d4ffb9cc9bae
rucci <d.rucci.2001@gmail.com>
2026-06-25 19:26:00 +0700

    Trainer Ticket Concert Template

---
+++ trainer-ticket-concert/routes/routes.go
@@ -0,0 +1,58 @@
+package routes
+
+import (
+	"materi-middleware-gorm/handler"
+	"materi-middleware-gorm/middleware"
+	"materi-middleware-gorm/repository"
+	"materi-middleware-gorm/service"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func SetupRouter(db *gorm.DB) *gin.Engine {
+	r := gin.Default()
+
+	// Global Middlewares
+	r.Use(middleware.ApiKeyAuth())
+	r.Use(middleware.RateLimiter(5))
+
+	// Initialize layers
+	concertRepo := repository.NewConcertRepository(db)
+	concertService := service.NewConcertService(concertRepo)
+	concertHandler := handler.NewConcertHandler(concertService)
+
+	ticketCategoryRepo := repository.NewTicketCategoryRepository(db)
+	ticketCategoryService := service.NewTicketCategoryService(ticketCategoryRepo, concertRepo)
+	ticketCategoryHandler := handler.NewTicketCategoryHandler(ticketCategoryService)
+
+	// Inisialisasi Lapisan Booking
+	customerRepo := repository.NewCustomerRepository(db)
+	bookingRepo := repository.NewBookingRepository(db)
+	bookingService := service.NewBookingService(db, bookingRepo, customerRepo)
+	bookingHandler := handler.NewBookingHandler(bookingService)
+
+	// Group routes
+	api := r.Group("/api/v1")
+	{
+		// Concerts routes
+		api.POST("/concerts", concertHandler.CreateConcert)
+		api.GET("/concerts", concertHandler.GetConcerts)
+		api.GET("/concerts/:id", concertHandler.GetConcertByID)
+		api.PUT("/concerts/:id", concertHandler.UpdateConcert)
+		api.DELETE("/concerts/:id", concertHandler.DeleteConcert)
+
+		// Ticket Categories routes
+		api.POST("/ticket-categories", ticketCategoryHandler.CreateTicketCategory)
+		api.GET("/ticket-categories", ticketCategoryHandler.GetTicketCategories)
+		api.GET("/ticket-categories/:id", ticketCategoryHandler.GetTicketCategoryByID)
+		api.PUT("/ticket-categories/:id", ticketCategoryHandler.UpdateTicketCategory)
+		api.DELETE("/ticket-categories/:id", ticketCategoryHandler.DeleteTicketCategory)
+
+		// Booking routes
+		api.POST("/bookings", bookingHandler.CreateBooking)
+		api.GET("/bookings/:id", bookingHandler.GetBookingByID)
+	}
+
+	return r
+}
